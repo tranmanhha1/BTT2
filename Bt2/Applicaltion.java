7df1ce2ab8299f12926be063a6e41b09fd17c573
tranmanhha1 <111476167+tranmanhha1@users.noreply.github.com>
2023-04-28 14:57:43 +0700

    qqde
    wfew

---
+++ Bt2/Applicaltion.java
@@ -0,0 +1,9 @@
+package Bt2;
+
+public class Applicaltion {
+    public static void main(String[] args) {
+        StudentManagementProgram test = new StudentManagementProgram();
+        test.run();
+    }
+}
+
